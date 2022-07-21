3ac3e8f26ad4341a511b2a9f033ed0839cf08f07
Raymond Wu <raymonwu@umich.edu>
2022-07-21 16:06:37 -0400

    Added movie overview padding

--- src/App.js
+++ src/App.js
@@ -1,62 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import Movie from './components/Movie';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Movie from "./components/Movie";
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
-  const [ movies, setMovies ] = useState([]);
-  const [ searchTerm, setSearchTerm ] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
 
   //fetch movies with api
-const getMovies = async (API) => {
-  // fetch(API)
-  // .then(res => res.json())
-  // .then(data => {
-  //     setMovies(data.results);
-  //     console.log(data);
-  // });
-  const res = await fetch(API)
-  const data = await res.json()
-  setMovies(data.results)
-}
-//search submit
+  const getMovies = async (API) => {
+    // fetch(API)
+    // .then(res => res.json())
+    // .then(data => {
+    //     setMovies(data.results);
+    //     console.log(data);
+    // });
+    const res = await fetch(API);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+  //search submit
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    if(searchTerm) {
-      getMovies(SEARCH_API + searchTerm)
+    if (searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
     }
-  }
+  };
 
   const handleOnChange = (e) => {
-    setSearchTerm(e.target.value); 
-  }
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <>
-    <header>
-      <form onSubmit={handleOnSubmit}>
-          <input className="search"         
-          type="search" 
-          placeholder="Search..." 
-          value={searchTerm}
-          onChange={handleOnChange}/>
-          </form>
-          <h1>Alex flix</h1>
-        </header>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+        <h1>Tao Flix</h1>
+      </header>
       <div className="movie-container">
-        {movies.length > 0 && 
-        movies.map((movie) => 
-          <Movie key={movie.id} {...movie}/>
-        )}
+        {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
       </div>
-      </>
+    </>
   );
 }
 
